1c2f4a8c82ce2e362a75e123aa32139ecb8dce64
Abhay Mishra <abhaymishra9945@gmail.com>
2026-06-28 19:33:37 +0530

    explore

--- src/app.js
+++ src/app.js
@@ -1,7 +1,15 @@
 const express = require("express");
 app = express();
-app.use("/", (req, res) => {
-  res.send("Hello World");
+app.get("/User", (req, res) => {
+  res.send({
+    firstName: "Abhay",
+    lastName: "Mishra",
+    age: 22,
+    city: "Bangalore",
+  });
+});
+app.use("/hello", (req, res) => {
+  res.send("Hello World hoo! This is my first nodejs project");
 });
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
